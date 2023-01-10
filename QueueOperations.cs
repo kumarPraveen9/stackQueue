e815d9db4bb8c5bb1aea33d27962ace0283d9f1f
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-10 21:48:49 +0530

    Queue operations using linked list

---
+++ QueueOperations.cs
@@ -0,0 +1,82 @@
+
+namespace Day14StaQueue
+{
+    class Program
+    {
+       public  class QueueNode
+        {
+          public  int data;
+          public  QueueNode next;
+            public QueueNode(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        QueueNode root;
+
+        public void Enqueue(int data)
+        {
+            QueueNode newNode = new QueueNode(data);
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                QueueNode temp = root;
+                root = newNode;
+                newNode.next = temp;
+            }
+            Console.WriteLine("Item Enqueueed into stack = " + data);
+        }
+
+        public int Dequeue()
+        {
+             int Dequeued;
+            if (root == null)
+            {
+                Console.WriteLine("Stack is Empty");
+                return 0;
+            }
+            else
+            {
+                while (root.next != null)
+                {
+                    
+                    root = root.next;
+                    
+                }
+                Dequeued = root.data;
+                return Dequeued;
+            }
+        }
+
+        public int peek()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return 0;
+            }
+            else
+            {
+                return root.data;
+            }
+        }
+
+        public static void Main(String[] args)
+        {
+            Program m = new Program();
+            m.Enqueue(10);
+            m.Enqueue(20);
+            m.Enqueue(30);
+          
+                Console.WriteLine("Item Dequeueped from stack = " + m.Dequeue());
+                Console.WriteLine(m.peek() + " Returned by Peek operation");
+            
+        }
+    }
+
+
+}
